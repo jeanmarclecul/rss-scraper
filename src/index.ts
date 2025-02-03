9243a843abd6f7975b5d65e48f87b50152483d68
jeanmarc LECUL <togashifr@outlook.fr>
2025-02-03 23:56:17 +0100

    feat(config): add new sources

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { newsSources } from "./config";
+import { newsSources } from "./scrapConfig";
 import { scrapeNews } from "./scraper";
 import { generateRssFeed } from "./rss";
 
